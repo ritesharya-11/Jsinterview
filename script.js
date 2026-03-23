3b9790b722a00667b6632ac0299e8cf010297b3d
Ritesh Kumar <ritesharya9955@gmail.com>
2026-03-24 02:16:24 +0530

    djd

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
-// map. filter and reduce - O/P 
-// question-1 Return only name of student in capital
-// Question2 - return only detail of whonscored more than 60 marks
+ // Function in javascript
+ // Q1- What is function declaration
 
-// Question3- more than 60 marks and roll number greater than 15
+ function square(num){
+    return num*num;
+    
+ }
+// what is function expression- store inside the function variable called functin expression
 
-// Question4 - Sum of marks of all students
+const square = function(num){
+    return num*num;
+}
+// what is anonymous function- no function name
 
-//  Question5- return only names of student who scored more than 60
-// Question6- Return total marks for students with marks greater than 60 after 20 marks have been
-//                   added to those student less than 60
+// Q3 - What are the First class Function
+     // function are treated like  values .
+     // you can store them, pass them return them
+   
+     function square(num){
+        return num *num;
+     }
 
-let students = [
-    { name: "ritesh",   rollnumber: 31, marks: 40 },
-    { name: "jeny",     rollnumber: 47, marks: 75 },  // ✅ added marks value
-    { name: "kaushal",  rollnumber: 45, marks: 90 },  // ✅ added marks value
-    { name: "kapoor",   rollnumber: 23, marks: 65 }   // ✅ fixed maeks → marks
-];
+     function displaySquare(fn){
+        console.log("Square is" +fn(4));
+        
+     }
 
-// const names = students.map((stu) => stu.name.toUpperCase());
+     displaySquare(square)
 
-// console.log(names);
-
-// const details  = students.filter((stu) => stu.marks > 60);
-
-// console.log(details);
-
-// const detail = students.filter((stu) => stu.marks>60 && stu. rollnumber>15)
-
-// console.log(detail);
-
-// const sum = students.reduce((acc,curr)=>acc+curr.marks,0)
-
-// console.log(sum);
-
-// const names = students.filter((stu) =>stu.marks > 60).map((stu)=>stu.name);
-// console.log(names);
-
-const totalMarks = students.map((stu) =>{
-    if(stu.marks < 60){
-        stu.marks += 20;
-    }
-    return stu;
-}).filter(stu=>stu.marks >60)
-
-console.log(totalMarks);
 
  
 
